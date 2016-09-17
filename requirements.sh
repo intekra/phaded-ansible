8b80e879b2044f34e2428621047f993470e5af82
Jason Witkowski <jwitko@saucelabs.net>
2016-09-17 13:00:05 -0400

    adding dist-upgrade for initial install

--- requirements.sh
+++ requirements.sh
@@ -6,5 +6,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 apt-get clean
 apt-get update
+apt-get dist-upgrade -y
 apt-get install -y wget git curl vim vim-enhanced 
 apt-get install -y python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev gcc libyaml-dev libpython2.7-dev
